abb1b58425620bac6a75ff24e413a55bfcb04fbb
ConorLynch <ctlynch89@live.com>
2025-03-09 00:39:04 +0000

    initial commit

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+# this is the deploy script for secure https
+sudo apt update && sudo apt install nodejs npm
+
+sudo npm install -g pm2
+
+pm2 stop example_app
+
+cd SimpleApplication/
+
+npm install
+
+echo $PRIVATE_KEY > privatekey.pem
+
+echo $SERVER > server.crt
+
+pm2 start ./bin/www --name example_app
